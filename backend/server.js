ee8e5169eb9e2b3f2fa3d1ffb5664239671da626
Pablo-Rodriguez <pablorp1996@gmail.com>
2017-09-03 01:07:32 +0200

    Bug arreglado

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 
 const http = require('http')
+const express = require('express')
 
 const Database = require('./lib/database')
 const App = require('./app')
 const config = require('./config/')
 
-const app = new App()
+const app = new App(express())
 const server = http.createServer(app.getRouter())
 
 Database.connect(config.db.url)
